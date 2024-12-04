c8ce68d0a2659354ecaacc9ad85c01ae909919f2
mirodildev <mirodil7671@gmail.com>
2024-12-04 17:14:00 +0500

    created data connection

---
+++ WAD.CODEBASE.00022145/WAD.CODEBASE.00022145/Data/GeneralDBContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WAD.CODEBASE._00022145.Models;
+
+namespace WAD.CODEBASE._00022145.Data
+{
+    public class GeneralDBContext : DbContext
+    {
+        public GeneralDBContext(DbContextOptions<GeneralDBContext> options) : base(options) { }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Event> Events { get; set; }
+
+        
+    }
+}
--- WAD.CODEBASE.00022145/WAD.CODEBASE.00022145/obj/Debug/net6.0/WAD.CODEBASE.00022145.AssemblyInfo.cs
+++ WAD.CODEBASE.00022145/WAD.CODEBASE.00022145/obj/Debug/net6.0/WAD.CODEBASE.00022145.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WAD.CODEBASE.00022145")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e8e340204913ed73cc85820969d1dd35b4cf83f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c660f98815070665480ed9f8c245f9abfbdcb080")]
 [assembly: System.Reflection.AssemblyProductAttribute("WAD.CODEBASE.00022145")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WAD.CODEBASE.00022145")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
