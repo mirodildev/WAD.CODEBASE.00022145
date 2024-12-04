8007d870bb71fa26f99a03a47d6bc2cd530c45cf
mirodildev <mirodil7671@gmail.com>
2024-12-04 17:47:03 +0500

    created controllers

---
+++ WAD.CODEBASE.00022145/WAD.CODEBASE.00022145/Data/Controllers/CategoryController.cs
@@ -0,0 +1,91 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WAD.CODEBASE._00022145.Models;
+using WAD.CODEBASE._00022145.Repositories;
+
+namespace WAD.CODEBASE._00022145.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class CategoryController : ControllerBase
+    {
+        private readonly IRepository<Category> _repository;
+        public CategoryController(IRepository<Category> repository)
+        {
+            _repository = repository;
+        }
+
+
+
+
+
+
+
+        
+        [HttpGet]
+        public async Task<IEnumerable<Category>> Get()
+        {
+            return await _repository.GetAllAsync();
+        }
+
+
+
+
+
+
+
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetByID(int id)
+        {
+            var resultedCategory = await _repository.GetByIDAsync(id);
+            if (resultedCategory == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(resultedCategory);
+            }
+        }
+
+
+
+
+
+
+
+        [HttpPost]
+        public async Task<IActionResult> Create(Category categories)
+        {
+            await _repository.AddAsync(categories);
+            return CreatedAtAction(nameof(GetByID), new { id = categories.ID }, categories);
+        }
+
+
+
+
+
+
+
+        [HttpPut]
+        public async Task<IActionResult> Update(Category categories)
+        {
+            //if(id!=items.ID) return BadRequest();
+            await _repository.UpdateAsync(categories);
+            return NoContent();
+        }
+
+
+
+
+
+
+        [HttpDelete("{id}")]
+        
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _repository.DeleteAsync(id);
+            return NoContent();
+        }
+    }
+}
---
+++ WAD.CODEBASE.00022145/WAD.CODEBASE.00022145/Data/Controllers/EventController.cs
@@ -0,0 +1,125 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WAD.CODEBASE._00022145.Models;
+using WAD.CODEBASE._00022145.Repositories;
+
+namespace WAD.CODEBASE._00022145.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class EventController : ControllerBase
+    {
+
+
+
+
+
+        private readonly IRepository<Event> _eventRepository;
+        public EventController(IRepository<Event> eventRepository)
+        {
+            _eventRepository = eventRepository;
+        }
+
+
+
+
+
+
+
+
+
+    
+        [HttpGet]
+        public async Task<IEnumerable<Event>> GetAll() => await _eventRepository.GetAllAsync();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        [HttpGet("{id}")]
+        [ProducesResponseType(typeof(Event), StatusCodes.Status200OK)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> GetByID(int id)
+        {
+            var resultedEvent = await _eventRepository.GetByIDAsync(id);
+            return resultedEvent == null ? NotFound() : Ok(resultedEvent);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        [HttpPost]
+        [ProducesResponseType(StatusCodes.Status201Created)]
+        public async Task<IActionResult> Create(Event items)
+        {
+            await _eventRepository.AddAsync(items);
+            return Ok(items);
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+
+        [HttpPut]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status400BadRequest)]
+        public async Task<IActionResult> Update(Event items) 
+        {
+            await _eventRepository.UpdateAsync(items);
+            return NoContent();
+        }
+
+
+
+
+
+
+
+
+
+
+
+
+        [HttpDelete("{id}")]
+        [ProducesResponseType(StatusCodes.Status204NoContent)]
+        [ProducesResponseType(StatusCodes.Status404NotFound)]
+        public async Task<IActionResult> Delete(int id)
+        {
+            await _eventRepository.DeleteAsync(id);
+            return NoContent(); 
+
+
+        }
+
+
+
+    }
+}
