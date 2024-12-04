881694dd43ebd41d352200217726dc478d8c88f0
mirodildev <mirodil7671@gmail.com>
2024-12-04 17:54:38 +0500

    updated programm.cs and appSettings

--- WAD.CODEBASE.00022145/WAD.CODEBASE.00022145/Program.cs
+++ WAD.CODEBASE.00022145/WAD.CODEBASE.00022145/Program.cs
@@ -1,25 +1,40 @@
+using Microsoft.EntityFrameworkCore;
+using WAD.CODEBASE._00022145.Data;
+using WAD.CODEBASE._00022145.Models;
+using WAD.CODEBASE._00022145.Repositories;
+
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+
+builder.Services.AddDbContext<GeneralDBContext>(
+    o => o.UseSqlServer(
+        builder.Configuration.GetConnectionString("SqlServerConnection")));
+
+builder.Services.AddScoped<IRepository<Event>, EventRepository>();
+builder.Services.AddScoped<IRepository<Category>, CategoryRepository>();
+
+
+
+
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
+
 if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
 }
 
-app.UseHttpsRedirection();
-
 app.UseAuthorization();
 
 app.MapControllers();
+
+
 
 app.Run();
--- WAD.CODEBASE.00022145/WAD.CODEBASE.00022145/obj/Debug/net6.0/WAD.CODEBASE.00022145.AssemblyInfo.cs
+++ WAD.CODEBASE.00022145/WAD.CODEBASE.00022145/obj/Debug/net6.0/WAD.CODEBASE.00022145.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WAD.CODEBASE.00022145")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e2737478d6bb1ab5bc501eeeac62c32d47cc7b48")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8007d870bb71fa26f99a03a47d6bc2cd530c45cf")]
 [assembly: System.Reflection.AssemblyProductAttribute("WAD.CODEBASE.00022145")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WAD.CODEBASE.00022145")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
